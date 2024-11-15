3752c1d900d092edf7664045dda452bc4036443c
Santos <santos.farias@mi.unc.edu.ar>
2024-11-15 16:59:44 -0300

    {M}ej 5 y modificaciones en el 4

--- lab01/ej5/array_helpers.h
+++ lab01/ej5/array_helpers.h
@@ -1,9 +1,12 @@
-#include <stdbool.h>
+#ifndef ARRAY_HELPERS_H /* Verificamos si ARRAY_HELPERS_H no ha sido definido previamente*/
+#define ARRAY_HELPERS_H /* Definimos ARRAY_HELPERS_H para que las siguientes inclusiones de este archivo no vuelvan a incluir su contenido */
 
-unsigned int array_from_file(int array[],
-                             unsigned int max_size,
-                             const char *filepath); 
+#include "mybool.h"
+
+unsigned int array_from_file(int array[], unsigned int max_size, const char *filepath); 
 
 void array_dump(int a[], unsigned int length);
 
-bool array_is_sorted(int a[], unsigned int length);
+mybool array_is_sorted(int a[], unsigned int length);
+
+#endif  /* Finaliza la condicion iniciada por ifndef */

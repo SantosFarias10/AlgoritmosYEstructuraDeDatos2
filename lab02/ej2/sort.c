b5913f42d4fb53c3a2be7da9ed5d9d8399da092d
Santos <santos.farias@mi.unc.edu.ar>
2024-11-18 18:23:41 -0300

    {M}ej2 terminado

--- lab02/ej2/sort.c
+++ lab02/ej2/sort.c
@@ -8,15 +8,21 @@
 
 
 static void quick_sort_rec(int a[], unsigned int izq, unsigned int der) {
- /* needs implementation */
-
-    /* no implementes partition, ya está implementado en sort_helpers.o
-       (no se puede leer, pero en sort_helpers.h vas a encontrar información
-        para saber cómo usarlo)
+    unsigned int pivot = partition(a, izq, der);
+    /* Permuta los elementos de a[izq..der] y devuelve el pivote tal que:
+        - izq <= pivote <= der
+        - los elementos en a[izq,pivote) todos 'van antes' (según la función goes_before) de a[pivote]
+        - a[pivote] 'va antes' de todos los elementos en a(pivote,der] 
     */
+
+    if (pivot > izq) {
+        quick_sort_rec(a, izq, pivot - 1u);
+    }
+    if(pivot < der) {
+        quick_sort_rec(a, pivot + 1u, der);
+    }
 }
 
 void quick_sort(int a[], unsigned int length) {
     quick_sort_rec(a, 0u, (length == 0u) ? 0u : length - 1u);
 }
-

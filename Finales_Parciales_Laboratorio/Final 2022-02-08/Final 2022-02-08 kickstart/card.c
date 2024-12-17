903b9d57a5bce0e9dd739dcce2f0061ee06743c4
Santos <santos.farias@mi.unc.edu.ar>
2024-12-17 12:55:09 -0300

    {M}Final 2022-02-08 terminado

--- Finales_Parciales_Laboratorio/Final 2022-02-08/Final 2022-02-08 kickstart/card.c
+++ Finales_Parciales_Laboratorio/Final 2022-02-08/Final 2022-02-08 kickstart/card.c
@@ -13,25 +13,28 @@ struct s_card {
 static bool
 invrep(card_t c) {
     // Invariante de representación
-    bool valid=false;
+    bool valid = false;
     valid = c != NULL;
-    /*
-     * [COMPLETAR]
-     *
-     * Mejorar la invariante chequeando más
-     * propiedades de los campos de la estructura
-     *
-     */
+
+    if(c != NULL && (c->num > 9u || c->color < 0 || c->type < 0 || c->color > 3 || c->type > 2)){
+        valid = false;
+    }
+
     return valid;
 }
 
 card_t
 card_new(unsigned int num, color_t color, type_t s) {
-    card_t card=NULL;
-    /*
-     * [COMPLETAR]
-     *
-     */
+    assert(num < 10 && (color == red || color == green || color == blue || color == yellow) && (s == normal || s == change_color || s == skip));
+    card_t card = NULL;
+
+    card = malloc(sizeof(struct s_card));
+
+    card->color = color;
+    card->num = num;
+    card->type = s;
+
+    assert(invrep(card));
     return card;
 }
 
@@ -56,38 +59,46 @@ card_get_type(card_t card) {
 
 bool
 card_samecolor(card_t c1, card_t c2) {
-    /*
-     * [COMPLETAR]
-     *
-     */
+    return(card_get_color(c1) == card_get_color(c2));
 }
 
 
 bool
 card_samenum(card_t c1, card_t c2) {
-    /*
-     * [COMPLETAR]
-     *
-     */
+    return (card_get_number(c1) == card_get_number(c2));
 }
 
 bool
 card_compatible(card_t new_card, card_t pile_card) {
-    bool compatible=false;
+    bool compatible = false;
     assert(invrep(new_card) && invrep(pile_card));
-    /*
-     * [COMPLETAR]
-     *
-     */
+
+    if(card_get_color(new_card) == card_get_color(pile_card)){
+        compatible = true;
+    }
+
+    if(card_get_type(new_card) == normal && card_get_type(pile_card) == normal){
+        if(card_get_number(new_card) == card_get_number(pile_card) || card_get_color(new_card) == card_get_color(pile_card)){
+            compatible = true;
+        }
+    }
+    if(card_get_type(new_card) == skip){
+        if(card_get_color(new_card) == card_get_color(pile_card) || card_get_type(pile_card) == skip){
+            compatible = true;
+        }
+    }
+
+    if(card_get_type(new_card) == change_color){
+        compatible = true;
+    }
+
     return compatible;
 }
 
+
 card_t
 card_destroy(card_t card) {
-    /*
-     * [COMPLETAR]
-     *
-     */
+    free(card);
+    card = NULL;
+    return NULL;
 }
-
-
--- Finales_Parciales_Laboratorio/Final 2022-02-08/Final 2022-02-08 kickstart/unogame.c
+++ Finales_Parciales_Laboratorio/Final 2022-02-08/Final 2022-02-08 kickstart/unogame.c
@@ -8,83 +8,131 @@
 #define NO_PLAYER -1
 
 struct s_ugame {
-    /*
-     * [COMPLETAR]
-     *
-     */
+    player_t player;
+    card_t card;
+    struct s_ugame *next;
 };
 
 unogame_t
 uno_newgame(card_t card) {
-    /*
-     * [COMPLETAR]
-     *
-     */
+    struct s_ugame *new = NULL;
+    new = malloc(sizeof(struct s_ugame));
+
+    new->card = card;
+    new->player = NO_PLAYER;
+    new->next = NULL;
+
+    return new;
 }
 
 card_t
 uno_topcard(unogame_t uno) {
-    /*
-     * [COMPLETAR]
-     *
-     */
+    return (uno->card);
 }
 
 unogame_t
 uno_addcard(unogame_t uno, card_t c) {
-    /*
-     * [COMPLETAR]
-     *
-     */
+    struct s_ugame *new = NULL;
+    new = malloc(sizeof(struct s_ugame));
+
+    new->card = c;
+    new->player = uno_nextplayer(uno);
+    new->next = uno;
+
+    return new;
 }
 
 unsigned int
 uno_count(unogame_t uno) {
-    /*
-     * [COMPLETAR]
-     *
-     */
+    unsigned int count = 0;
+    struct s_ugame *aux = uno;
+
+    while (aux != NULL) {
+        count++;
+        aux = aux->next;
+    }
+
+    return count;
 }
 
 bool
 uno_validpile(unogame_t uno) {
-    /*
-     * [COMPLETAR]
-     *
-     */
+    bool res = true;
+    unogame_t aux = uno;
+
+    while(aux->next != NULL && res){
+        if(!card_compatible(aux->card, aux->next->card)){
+            res = false;
+        }
+        aux = aux->next;
+    }
+
+    if(card_get_type(aux->card) == change_color){
+        res = false;
+    }
+
+    return res;
 }
 
 color_t
 uno_currentcolor(unogame_t uno) {
-    /*
-     * [COMPLETAR]
-     *
-     */
+    return card_get_color(uno->card);
 }
 
 player_t
 uno_nextplayer(unogame_t uno) {
-    /*
-     * [COMPLETAR]
-     *
-     */
+    player_t nextp;
+    assert(uno!= NULL);
+
+    if(uno->player == NUM_PLAYERS-1){
+        if(card_get_type(uno->card) == skip){
+            nextp = 1;
+        }else{
+            nextp = 0;
+        }
+    } else {
+        if(card_get_type(uno->card) == skip){
+            if(uno->player + 1 == NUM_PLAYERS-1){
+                nextp = 0;
+            }else{
+                nextp = uno->player + 2;
+            }
+        }else{
+            nextp = uno->player + 1;
+        }
+    }
+
+    assert(nextp < NUM_PLAYERS && nextp >= 0);
+    return nextp;
 }
 
 
 
 card_t *
 uno_pile_to_array(unogame_t uno) {
-    /*
-     * [COMPLETAR]
-     *
-     */
+    card_t *array = calloc(uno_count(uno), sizeof(card_t));
+    unogame_t aux = uno;
+    
+    for(unsigned int i = uno_count(uno); i > 0u && aux != NULL; i--){
+        array[i - 1] = aux->card;
+        aux = aux->next;
+    }
+
+    return array;
 }
 
 unogame_t
 uno_destroy(unogame_t uno) {
-    /*
-     * [COMPLETAR]
-     *
-     */
+    unogame_t aux;
+    while(uno != NULL){
+        aux = uno;
+        uno = uno->next;
+        aux->card = card_destroy(aux->card);
+        free(aux);
+    }
+
+    uno = NULL;
+
+    return uno;
 }
 
